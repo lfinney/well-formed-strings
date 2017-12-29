756d35e783bc13a59643303d8618365836c371f0
Luke Finney <lucaswfinney@gmail.com>
2017-12-29 12:55:42 -0600

    Intial rendition of program

--- balancedFunction-test.js
+++ balancedFunction-test.js
@@ -2,7 +2,74 @@ const assert = require('chai').assert;
 const balancedFunction = require('./balancedFunction');
 
 describe('Balanced Functions', () => {
+  let string;
+
   it('should be a function', () => {
     assert.isFunction(balancedFunction)
-  })
+  });
+
+  it('should return true if parenthesis are balanced, ()', () => {
+    string = '()'
+    assert.equal(balancedFunction(string), true)
+  });
+
+  it('should return true if parenthesis are balanced, {}', () => {
+    string = '{}'
+    assert.equal(balancedFunction(string), true)
+  });
+
+  it('should return true if parenthesis are balanced, []', () => {
+    string = '[]'
+    assert.equal(balancedFunction(string), true)
+  });
+
+  it('should return false if parenthesis are unbalanced, )(', () => {
+    string = ')('
+    assert.equal(balancedFunction(string), false)
+  });
+
+  it('should return false if parenthesis are unbalanced, }{', () => {
+    string = '}{'
+    assert.equal(balancedFunction(string), false)
+  });
+
+  it('should return false if parenthesis are unbalanced, ][', () => {
+    string = ']['
+    assert.equal(balancedFunction(string), false)
+  });
+
+  it('should return true if parenthesis are balanced, (())', () => {
+    string = '(())'
+    assert.equal(balancedFunction(string), true)
+  });
+
+  it('should return false if parenthesis are unbalanced, (()', () => {
+    string = '(()'
+    assert.equal(balancedFunction(string), false)
+  });
+
+  it('should return false if parenthesis are unbalanced, ({[)}]', () => {
+    string = '([{'
+    assert.equal(balancedFunction(string), false)
+  });
+
+  it('should return true if parenthesis are balanced, ({[]})', () => {
+    string = '({[]})'
+    assert.equal(balancedFunction(string), true)
+  });
+
+  it('should return false if parenthesis are unbalanced, ({[)}]', () => {
+    string = '({[)}]'
+    assert.equal(balancedFunction(string), false)
+  });
+
+  it('should return true if parenthesis are balanced, ({()[]}[()]{})', () => {
+    string = '({()[]}[()]{})'
+    assert.equal(balancedFunction(string), true)
+  });
+
+  it('should return false if parenthesis are unbalanced, [((){)}]', () => {
+    string = '[((){)}]'
+    assert.equal(balancedFunction(string), false)
+  });
 });
--- balancedFunction.js
+++ balancedFunction.js
@@ -1,5 +1,33 @@
-const balancedFunction = (items) => {
-  console.log('whodis?');
+const balancedFunction = (string) => {
+  const stack = [];
+  const opened = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+
+  const closed = {
+    ')': true,
+    ']': true,
+    '}': true
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    let puncuation = string[i];
+
+    if (opened[puncuation]) {
+      stack.push(puncuation)
+    }
+
+    if (closed[puncuation]) {
+
+      if (opened[stack.pop()] !== puncuation) {
+        return false
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
 
 module.exports = balancedFunction;
